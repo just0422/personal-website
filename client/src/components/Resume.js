38d2b07c870c28460e90704dd1446de1aa9dd0c8
Justin Maldonado <justin.maldonado@stonybrook.edu>
2018-12-05 22:04:31 -0500

    Added contact info and started Education

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { PageHeader } from 'react-bootstrap';
+import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
 
-import Skills from './Resume/Skills'
+import Skills from './Resume/Skills';
+import Education from './Resume/Education';
 
 export default class Resume extends Component {
 	render(){
 		return(
 			<div className="container">
-				<PageHeader>Resume</PageHeader>
-				<Skills />
+				<PageHeader>Justin Maldonado</PageHeader>
+				<Grid>
+					<Row>
+						<Col xs={4}>(347) 922-5075</Col>
+						<Col xs={4}><a href="mailto:justin.maldonado@stonybrook.edu">justin.maldonado@stonybrook.edu</a></Col>
+						<Col xs={4}><a href="https://github.com/just0422">https://github.com/just0422</a></Col>
+					</Row>
+					<Row>
+						<Col xs={6}>
+							<Education />
+						</Col>
+						<Col xs={6}>
+							<Skills />
+						</Col>
+					</Row>
+				</Grid>
 			</div>
 		);
 	}
---
+++ client/src/components/Resume/Education.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Education extends Component {
+	render() {
+		return (
+			<div>
+				<h3 className="resume-subheader">Education</h3>
+			</div>
+		);
+	}
+}
