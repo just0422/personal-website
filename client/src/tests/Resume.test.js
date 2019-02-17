b74b504b1b876e76cdeaa3e14c5fde5a605c5fc5
Justin Maldonado <justin.maldonado@stonybrook.edu>
2019-02-17 18:45:05 -0500

    Added resume succsses and fails

--- client/src/tests/Resume.test.js
+++ client/src/tests/Resume.test.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
 import Resume from 'Resume';
+import Education from 'Resume/Education';
+import Skills from 'Resume/Skills';
+import Experience from 'Resume/Experience';
 import { skillsUrl, experiencesUrl } from 'APIUtils';
 import { skills, experiences } from './data';
 
@@ -14,22 +17,57 @@ describe('Resume test', () => {
 	const skillsFail = () => mock.onGet(skillsUrl).reply(404, 'No skills available');
 	const skillsSucceed = () => mock.onGet(skillsUrl).reply(200, skills);
 	const experiencesFail = () => mock.onGet(experiencesUrl).reply(404, 'No expriences available');
-	const experiencesSucceed = () => mock.onGet(experiencesSucceed).reply(200, experiences);
+	const experiencesSucceed = () => mock.onGet(experiencesUrl).reply(200, experiences);
 
 	beforeEach(() => mock = new MockAdapter(axios));
 
-	it('should shallow render correctly', () => {
+	it('should shallow render correctly', async () => {
 		skillsSucceed();
 		experiencesSucceed();
 
 		const component = shallow(<Resume />);
+		await flushPromises();
+
 		expect(component).toMatchSnapshot();
 		expect(component.state('error')).toBeNull();
 	});
 
-	it('should mount correctly with no props', () => {
+	it('should mount correctly', async () => {
+		skillsSucceed();
+		experiencesSucceed();
+
 		const component = mount(<Resume />);
+		component.update();
+		await flushPromises();
+
 		expect(component).toMatchSnapshot();
 		expect(component.state('error')).toBeNull();
+
+		expect(component.containsAllMatchingElements([
+			<Education />,
+			<Skills />
+		])).toBe(true);
+	});
+
+	it('should handle skills error', async () => {
+		skillsFail();
+		experiencesSucceed();
+
+		const component = mount(<Resume />);
+		await flushPromises();
+		
+		expect(component).toMatchSnapshot();
+		expect(component.state('error')).toBeTruthy();
+	});
+
+	it('should handle experiences error', async () => {
+		skillsSucceed();
+		experiencesFail();
+
+		const component = mount(<Resume />);
+		await flushPromises();
+		
+		expect(component).toMatchSnapshot();
+		expect(component.state('error')).toBeTruthy();
 	});
 });
