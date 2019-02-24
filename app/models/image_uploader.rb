b6d31ca1d4b1b3597d60895928e10d76413bd3a1
Justin Maldonado <justin.maldonado@stonybrook.edu>
2019-02-24 16:03:19 -0500

    Shrine validations

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -1,2 +1,6 @@
 class ImageUploader < Shrine
+	Attacher.validate do
+    validate_max_size 10.megabyte, message: "is too large (max is 10 MB)"
+    validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png']
+	end
 end
