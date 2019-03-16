583c6941038f1bee9e360acc6743493f24f9e985
Justin Maldonado <justin.maldonado@stonybrook.edu>
2019-03-16 11:51:36 -0400

    Added countdown message to reset

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {PageHeader, Grid} from 'react-bootstrap';
 import {Lightbox} from 'react-modal-image';
+import Countdown from 'react-countdown-now';
 
 import Project from 'Projects/Project';
 import ErrorModal from 'Error';
@@ -10,8 +11,9 @@ import 'stylesheets/projects.scss';
 export default class Projects extends Component {
   constructor(props) {
     super(props);
-	
-		this.updateProject = this.updateProject.bind(this);
+
+    this.resetTime = this.resetTime.bind(this);
+    this.updateProject = this.updateProject.bind(this);
     this.handleLightboxOpen = this.handleLightboxOpen.bind(this);
     this.handleLightboxClose = this.handleLightboxClose.bind(this);
 
@@ -20,6 +22,7 @@ export default class Projects extends Component {
       error: null,
       lightboxEnabled: false,
       lightboxImageSrc: '',
+      countDownTo: 0,
     };
   }
 
@@ -44,19 +47,29 @@ export default class Projects extends Component {
     this.setState({
       lightboxEnabled: false,
     });
-	}
+  }
 
-	updateProject(project){
-		this.setState({
-			projects: this.state.projects.map((state_project, i) => {
-				if (project.id === state_project.id)
-					return project
-				return state_project
-			})
-		});
-	}
+  updateProject(project) {
+    this.setState({
+      projects: this.state.projects.map((state_project, i) => {
+        if (project.id === state_project.id) return project;
+        return state_project;
+      }),
+    });
+  }
 
-	componentDidMount() {
+  resetTime() {
+    const interval = 900000; // 15 min
+    let time = Date.now();
+    let remainder = time % interval;
+
+    this.setState({
+      countDownTo: time + interval - remainder,
+    });
+  }
+
+  componentDidMount() {
+    this.resetTime();
     api
       .projects()
       .getAll()
@@ -65,7 +78,7 @@ export default class Projects extends Component {
           projects: response.data,
         });
       })
-			.catch(err => {
+      .catch(err => {
         this.setState({
           error: err,
         });
@@ -73,7 +86,7 @@ export default class Projects extends Component {
   }
 
   render() {
-		if (this.state.error) {
+    if (this.state.error) {
       return <ErrorModal component="Projects" error={this.state.error} />;
     } else {
       return (
@@ -86,15 +99,28 @@ export default class Projects extends Component {
               hideDownload={true}
             />
           )}
+          <div className="website-interactive-message">
+            This website is interactive. Any changes made will be reset every 15
+            minutes. Click on a project name to update it. Click on any bullet
+            point to delete it.
+            <br />
+            Restting in{' '}
+            <Countdown
+              date={this.state.countDownTo}
+              onComplete={() => this.resetTime()}
+              precision={2}
+              zeroPadDays={0}
+            />
+          </div>
           <PageHeader>Projects</PageHeader>
           <Grid>
             {this.state.projects.map((project, i) => {
               return (
                 <Project
                   project={project}
-									key={i}
-									openLightbox={this.handleLightboxOpen}
-									updateProject={this.updateProject}
+                  key={i}
+                  openLightbox={this.handleLightboxOpen}
+                  updateProject={this.updateProject}
                 />
               );
             })}
