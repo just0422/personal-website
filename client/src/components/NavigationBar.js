55d95252f420360a3ebe71c3710442cf2d130c45
Justin Maldonado <justin.maldonado@stonybrook.edu>
2019-03-17 23:52:53 -0400

    Prettiefied NavigationBar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { Navbar, NavItem, Nav, Button  } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Navbar, NavItem, Nav, Button} from 'react-bootstrap';
 import Countdown from 'react-countdown-now';
 
-import 'stylesheets/website.scss'
+import 'stylesheets/website.scss';
 
 export default class NavigationBar extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.resetTime = this.resetTime.bind(this);
+    this.resetTime = this.resetTime.bind(this);
 
-		this.state = {
-			countDownTo: 0,
-		}
-	}
+    this.state = {
+      countDownTo: 0,
+    };
+  }
 
   resetTime(complete) {
     const interval = 900000; // 15 min
@@ -22,54 +22,54 @@ export default class NavigationBar extends Component {
 
     this.setState({
       countDownTo: time + interval - remainder,
-		});
-	}
+    });
+  }
 
-	componentDidMount(){
-		this.resetTime(false);
-	}
+  componentDidMount() {
+    this.resetTime(false);
+  }
 
-	render () {
-		return (
-			<div>
-				<Navbar inverse>
-					<Navbar.Header>
-						<Navbar.Brand>
-							<a href="/">Justin Maldonado</a>
-						</Navbar.Brand>
-						<Navbar.Toggle />
-					</Navbar.Header>
-					<Navbar.Collapse>
-						<Nav pullRight>
-							<NavItem eventKey={1} href="/">
-								Resume
-							</NavItem>
-							<NavItem eventKey={2} href="/projects">
-								Projects
-							</NavItem>
-							<NavItem eventKey={3} href="/about">
-								About
-							</NavItem>
-							<NavItem eventKey={4} href="/contact">
-								Contact
-							</NavItem>
-							<NavItem eventKey={5} href="/upload">
-								Upload
-							</NavItem>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-				<div className="website-reset-countdown">
-					<Button onClick={this.props.handleLoading}>Reset Now</Button>{' '}
-					Reseting in{' '}
-					<Countdown
-						date={this.state.countDownTo}
-						onComplete={() => this.resetTime(true)}
-						precision={2}
-						zeroPadDays={0}
-					/>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Justin Maldonado</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="/">
+                Resume
+              </NavItem>
+              <NavItem eventKey={2} href="/projects">
+                Projects
+              </NavItem>
+              <NavItem eventKey={3} href="/about">
+                About
+              </NavItem>
+              <NavItem eventKey={4} href="/contact">
+                Contact
+              </NavItem>
+              <NavItem eventKey={5} href="/upload">
+                Upload
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="website-reset-countdown">
+          <Button onClick={this.props.handleLoading}>Reset Now</Button> Reseting
+          in{' '}
+          <Countdown
+            date={this.state.countDownTo}
+            onComplete={() => this.resetTime(true)}
+            precision={2}
+            zeroPadDays={0}
+          />
+        </div>
+      </div>
+    );
+  }
 }
