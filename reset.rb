a6781b34926f8db0fe0aaa315d3bd2b1416eada7
justin <just0422@gmail.com>
2019-03-20 19:08:32 +0000

    Fixed the reset script

--- reset.rb
+++ reset.rb
@@ -4,37 +4,27 @@ require 'json'
 require 'optparse'
 require 'rest-client'
 
-options = {}
-optparse = OptionParser.new do|opts|
-	options[:environment] = "development"
-	opts.on('-e', '--environment', 'Rails environment to send data to') do |env|
-		options[:environment] = env
-	end
+environment = "production"
+host = "localhost"
 
-	options[:host] = "localhost"
-	opts.on('-a', '--host', 'Server hostname') do |host|
-		options[:host] = host
-	end
+puts "Seeding " + environment + " Database"
 
-	options[:port] = 3001
-	opts.on('p', '--port', 'Server port') do |port|
-		options[:port] = port
-	end
-end
-optparse.parse!
-
-puts "Seeding " + options[:environment] + " Database"
-
-seed_database = "rails db:seed"
+seed_database = "RAILS_ENV=" + environment + " rails db:seed"
 puts "Running command: '" + seed_database + "'"
 
 system(seed_database)
 
-base_url="http://" + options[:host] + ":" + options[:port].to_s + "/api/v1/projects"
+base_url="http://" + host + "/api/v1/projects"
 puts "Getting projects from " + base_url
 projects = JSON.parse(RestClient.get(base_url))
 
 projects.each do |project|
+	project_found = " not "
+	if Dir.entries('screenshots').include? project["name"] 
+		project_found = " "
+	end
+
+	puts project["name"] + project_found + "found"
 	if Dir.entries('screenshots').include? project["name"]
 		url = base_url + "/" + project["id"].to_s + '/screenshots'
 		
